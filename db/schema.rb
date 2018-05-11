8acd680533a73b3feb0c82d25db70d3634a3ed86
n0dam1 <minoru.noda.0527@gmail.com>
2018-05-11 15:47:22 +0900

    リレーションを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_095531) do
+ActiveRecord::Schema.define(version: 2018_05_11_064455) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(version: 2018_05_09_095531) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "article_id"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -42,4 +46,6 @@ ActiveRecord::Schema.define(version: 2018_05_09_095531) do
   end
 
   add_foreign_key "articles", "users"
+  add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
